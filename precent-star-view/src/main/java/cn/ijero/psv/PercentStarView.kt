faa4576522113894135d5bfe444fe1df063e85db
ijero <ijero@qq.com>
2018-07-09 19:42:10 +0800

    正在开发对外接口

--- precent-star-view/src/main/java/cn/ijero/psv/PercentStarView.kt
+++ precent-star-view/src/main/java/cn/ijero/psv/PercentStarView.kt
@@ -2,6 +2,7 @@ package cn.ijero.psv
 
 import android.content.Context
 import android.graphics.*
+import android.support.annotation.FloatRange
 import android.util.AttributeSet
 import android.view.View
 import org.jetbrains.anko.AnkoLogger
@@ -16,43 +17,150 @@ constructor(
         attrs: AttributeSet? = null,
         defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr), AnkoLogger {
-    private var mCount = 5
-    private var mMax = 100
-    private var mProgress = 100
-    private var mStarColor = Color.parseColor("#FFFFFFFF")
-    private var mProgressColor = Color.YELLOW
-    private var mStrokeWidth = 1F
-    private var mStrokeColor = Color.parseColor("#FF666666")
+
+    companion object {
+        const val MIN_PLUMP = 0.3F
+        const val DEF_PLUMP = 0.4F
+        const val MAX_PLUMP = 0.8F
+        const val INT_INVALID = -1
+    }
+
+    var count = 5
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    var max = 100
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    var progress = 60
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    var starBackColor = Color.parseColor("#FFFFFFFF")
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    var starProgressColor = Color.RED
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    var starStrokeWidth = 1
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    var starStrokeColor = Color.parseColor("#FF666666")
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    var starSize = INT_INVALID
+        set(value) {
+            field = value
+            mOuterRadius = (field / 2).toFloat()
+        }
+
+
+    @FloatRange(from = MIN_PLUMP.toDouble(), to = MAX_PLUMP.toDouble())
+    var plump = DEF_PLUMP
+        set(value) {
+            field = value
+            mInnerRadius = mOuterRadius * field
+        }
+
     private var mOuterRadius = 0F
+        set(value) {
+            field = value
+            invalidate()
+        }
+
     private var mInnerRadius = 0F
+        set(value) {
+            field = value
+            invalidate()
+        }
 
     private var mStrokePaint = Paint().apply {
         style = Paint.Style.STROKE
         isAntiAlias = true
-        color = mStrokeColor
-        strokeWidth = mStrokeWidth
+        color = starStrokeColor
+        strokeWidth = this@PercentStarView.starStrokeWidth.toFloat()
     }
 
     private var mStarPaint = Paint().apply {
         style = Paint.Style.FILL
         isAntiAlias = true
-        strokeWidth = mStrokeWidth
-        color = mStarColor
+        strokeWidth = strokeWidth
+        color = starBackColor
     }
 
-    private var mStarSize = 0
+
+    private val mLayerRectF by lazy {
+        RectF(0F, 0F, measuredWidth.toFloat(), measuredHeight.toFloat())
+    }
 
     init {
         setLayerType(View.LAYER_TYPE_SOFTWARE, null)
         applyStyle(attrs)
+        checkValues()
+    }
+
+    private fun checkValues() {
+        if (count < 0) {
+            throw IllegalArgumentException("The 'count' should > 0 .")
+        }
+
+        if (plump < MIN_PLUMP || plump > MAX_PLUMP) {
+            throw IllegalArgumentException("The 'plump' must be range in '0.4' to '0.8'.")
+        }
+
+        if (progress < 0) {
+            progress = 0
+        }
+
+        if (progress > max) {
+            progress = max
+        }
+
+        if (starSize == INT_INVALID) {
+            starSize = dip(30)
+        }
+
+        mOuterRadius = starSize.toFloat() * 0.5F
+        mInnerRadius = mOuterRadius * plump
+
+
     }
 
     private fun applyStyle(attrs: AttributeSet?) {
         attrs ?: return
 
-        mStarSize = dip(50)
-        mOuterRadius = (mStarSize / 2).toFloat()
-        mInnerRadius = (mStarSize / 5).toFloat()
+        val ta = context.obtainStyledAttributes(attrs, R.styleable.PercentStarView)
+        starBackColor = ta.getColor(R.styleable.PercentStarView_psv_starBackColor, starBackColor)
+        starProgressColor = ta.getColor(R.styleable.PercentStarView_psv_starProgressColor, starProgressColor)
+        starStrokeColor = ta.getColor(R.styleable.PercentStarView_psv_starStrokeColor, starStrokeColor)
+        starStrokeWidth = ta.getDimensionPixelSize(R.styleable.PercentStarView_psv_starStrokeWidth, starStrokeWidth)
+        count = ta.getInt(R.styleable.PercentStarView_psv_starCount, count)
+        plump = ta.getFloat(R.styleable.PercentStarView_psv_starPlump, plump)
+        starSize = ta.getDimensionPixelSize(R.styleable.PercentStarView_psv_starSize, starSize)
+        max = ta.getInt(R.styleable.PercentStarView_psv_max, max)
+        progress = ta.getInt(R.styleable.PercentStarView_psv_progress, progress)
+
+        ta.recycle()
+
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -64,7 +172,7 @@ constructor(
         val size = MeasureSpec.getSize(widthMeasureSpec)
 
         if (mode == MeasureSpec.AT_MOST) {
-            return mStarSize * mCount + paddingLeft + paddingRight
+            return starSize * count + paddingLeft + paddingRight
         }
 
         return size
@@ -75,7 +183,7 @@ constructor(
         val size = MeasureSpec.getSize(heightMeasureSpec)
 
         if (mode == MeasureSpec.AT_MOST) {
-            return mStarSize + paddingBottom + paddingTop
+            return starSize + paddingBottom + paddingTop
         }
 
         return size
@@ -84,10 +192,11 @@ constructor(
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
+        val saveLayer = canvas.saveLayer(mLayerRectF, mStarPaint, Canvas.ALL_SAVE_FLAG)
         // starBack
-        canvas.translate(0F, mStarSize * 0.05f) // 减少上下和左右的间距差
+        canvas.translate(0F, starSize * 0.05f) // 减少上下和左右的间距差
         canvas.save()
-        mStarPaint.color = mStarColor
+        mStarPaint.color = starBackColor
         canvas.drawStars(mStarPaint)
         canvas.restore()
         canvas.save()
@@ -96,26 +205,30 @@ constructor(
         canvas.drawStars(mStrokePaint)
         canvas.restore()
 
-//        canvas.drawProgress()
+        canvas.drawProgress()
+        canvas.restoreToCount(saveLayer)
     }
 
     private fun Canvas.drawProgress() {
-        mStarPaint.color = mProgressColor
-        mStarPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_IN)
+        mStarPaint.color = starProgressColor
+
+        mStarPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
 
         drawRect(RectF(paddingStart.toFloat(), paddingTop.toFloat(),
                 (measuredWidth - paddingEnd).toFloat() - 240,
                 (measuredHeight - paddingBottom).toFloat()), mStarPaint)
+
         mStarPaint.xfermode = null
+
     }
 
     private fun Canvas.drawStars(paint: Paint) {
 
-        for (i in 0 until mCount) {
+        for (i in 0 until count) {
             if (i == 0) {
                 translate(paddingStart.toFloat(), paddingTop.toFloat())
             } else {
-                translate(mStarSize.toFloat(), 0F)
+                translate(starSize.toFloat(), 0F)
             }
             drawStar(paint)
         }
@@ -165,5 +278,6 @@ constructor(
                 (measuredWidth - paddingEnd).toFloat(),
                 (measuredHeight - paddingBottom).toFloat())
     }
+
 
 }
